569269644ddebf02acf5c8bd2a28a2483e247992
Arthh <arthur.ulhoa28@gmail.com>
2024-03-20 15:14:45 -0300

    check: release v0.3.14

--- core/class/entities/Grant.js
+++ core/class/entities/Grant.js
@@ -79,7 +79,7 @@ class Grant extends Attestation_1.Attestation {
             payload.push([this.details, await this.details.payloadFor(grantIdx)]);
         }
         if (this.milestones.length) {
-            await Promise.all(this.milestones.map(async (m) => payload.push(...(await m.multiAttestPayload(currentPayload, grantIdx)))));
+            await Promise.all(this.milestones.map(async (m) => payload.push(...(await m.multiAttestPayload(payload, grantIdx)))));
         }
         if (this.updates.length) {
             await Promise.all(this.updates.map(async (u) => payload.push([u, await u.payloadFor(grantIdx)])));
