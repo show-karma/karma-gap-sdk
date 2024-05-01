bd6dea38939985d61ae7e6eaf9ee2c2e91d41b2d
Mahesh Murthy <mahesh@mmurthy.com>
2024-05-01 12:27:51 -0700

    celo config

--- core/consts.ts
+++ core/consts.ts
@@ -203,6 +203,39 @@ export const Networks: Record<TNetwork, EASNetworkConfig> = {
         "0x5ddd6b7a11406771308431ca9bd146cc717848b74b52993a532dc1aad0ccc83f",
     },
   },
+  "celo": {
+    chainId: 42220,
+    url: "https://celo.easscan.org/graphql",
+    rpcUrl:
+      "https://forno.celo.org",
+    contracts: {
+      eas: "0x72E1d8ccf5299fb36fEfD8CC4394B8ef7e98Af92",
+      schema: "0x5ece93bE4BDCF293Ed61FA78698B594F2135AF34",
+      multicall: "0x8791Ac8c099314bB1D1514D76de13a1E80275950",
+      projectResolver: "0x6dC1D6b864e8BEf815806f9e4677123496e12026",
+      communityResolver: "0xfddb660F2F1C27d219372210745BB9f73431856E",
+    },
+    schemas: {
+      Community:
+        "0x3c2231024f4f17f3718b5bd9ed9ff29cc323dea5449f9ceba11a9888bfbdd0e1",
+      Details:
+        "0x9895e82115987d8e3e02b35ced92e6a0509293890333f58f50ec291b34853dac",
+      Grant:
+        "0x7afa603a89cee2d8f93d30007e2c64efddc6509fd76aa95d2ccd97b6e34acc71",
+      GrantVerified:
+        "0xf45fdf2c064073f0623416571c2746085d785cde5a57fd0696ff88bdf78bcbdc",
+      MemberOf:
+        "0xb4186a2401f40a4c78768941ef9140e1fbe5fe595053a65d44f31d6df180b712",
+      MilestoneApproved:
+        "0xf45fdf2c064073f0623416571c2746085d785cde5a57fd0696ff88bdf78bcbdc",
+      MilestoneCompleted:
+        "0xf45fdf2c064073f0623416571c2746085d785cde5a57fd0696ff88bdf78bcbdc",
+      GrantUpdateStatus:
+        "0xf45fdf2c064073f0623416571c2746085d785cde5a57fd0696ff88bdf78bcbdc",
+      Project:
+        "0xf3f753b41e04d1052b5a5ec7624d1dfdb6c2da288a985120e477ddbcac071022",
+    },
+  }
 } as const;
 
 const DetailsSchema = [{ type: "string", name: "json", value: null }];
