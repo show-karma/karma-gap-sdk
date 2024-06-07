62e1add576d32f0f18d267b2d5efd9b5bf9aecdf
Arthh <arthur.ulhoa28@gmail.com>
2024-06-07 08:56:36 -0300

    dev: create profile

--- core/scripts/create-program.ts
+++ core/scripts/create-program.ts
@@ -4,37 +4,34 @@ import { Networks } from "../consts";
 import { NFTStorage } from "nft.storage";
 import keys from "../../config/keys.json";
 
-const networkName = "sepolia"; // Only supported on testnet sepolia (indexer setup)
-// const web3 = new ethers.JsonRpcProvider(Networks[networkName].rpcUrl);
-// const wallet = new ethers.Wallet(keys.privateKey, web3);
-// const signer = wallet.connect(web3);
+const networkName = "sepolia"; 
+const web3 = new ethers.JsonRpcProvider(Networks[networkName].rpcUrl);
+const wallet = new ethers.Wallet(keys.privateKey, web3);
+const signer = wallet.connect(web3);
 
-export async function main(metadata: any, nonce: number, ipfsToken: string, signer: ethers.Wallet) {
+export async function main() {
   const ipfsStorage = new NFTStorage({
     token: keys.ipfsToken,
   });
 
   const alloRegistry = new AlloRegistry(signer, ipfsStorage);
 
-  // const nonce = 12310;
-  // const name = "Karma Test Program #011";
-  // const metadata = {
-  //   title: name,
-  //   description: `Karma Test Program #011`,
-  //   website: "https://karma.fund#011",
-  //   projectTwitter: "karma_fund11",
-  //   logoImg: "bafkreigf5egjxs3zbafr4d24kj5tf7idktfjc737xolx5h6dj7hnf77ndea",
-  //   bannerImg: "bafkreid5rccqi6q4xgv4cj5ofrzfmswf7fn2525e5hducvh3r56lstoi74a",
-  //   logoImgData: {},
-  //   bannerImgData: {},
-  //   credentials: {},
-  //   createdAt: new Date().getTime(),
+  const nonce = 12310;
+  const name = "Karma Gap Registry";
+  const metadata = {
+    title: name,
+    description: `Karma Gap Registry`,
+    website: "gap.karmahq.xyz",
+    projectTwitter: "karmahq_",
+    logoImg: "gap.karmahq.xyz/logo/karma-gap-logo.svg",
+    bannerImg: "gap.karmahq.xyz/logo/karma-gap-logo.svg",
+    logoImgData: {},
+    bannerImgData: {},
+    credentials: {},
+    createdAt: new Date().getTime(),
 
-    // TODO: Additional metadata
-    // category: "dapps",
-    // source: "grant-program-registry",
-    // ownerBoss: "arthur"
-  // };
+    type: "program"
+  };
   const owner = await signer.getAddress();
 
   const response = await alloRegistry.createProgram(
@@ -47,4 +44,4 @@ export async function main(metadata: any, nonce: number, ipfsToken: string, sign
   console.log(response);
 }
 
-// main();
+main();
