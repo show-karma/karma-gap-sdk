8dc19dc5de455bc6b761a5fae19bb67f5ce84098
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2024-08-27 18:16:38 -0300

    feat/test isolatedModules

--- core/class/entities/GrantUpdate.d.ts
+++ core/class/entities/GrantUpdate.d.ts
@@ -7,6 +7,7 @@ export interface IGrantUpdate {
     title: string;
     text: string;
     type?: string;
+    proofOfWork?: string;
 }
 type IStatus = "verified";
 export interface IGrantUpdateStatus {
