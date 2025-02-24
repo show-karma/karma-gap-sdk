df627d553cad2e8d0591e54d66f872445b3f6c0e
Fabian Ferno <fabianferno@gmail.com>
2025-02-25 02:22:36 +0530

    Pushing dist

---
+++ core/class/karma-indexer/api/GapIndexerApi.d.ts
@@ -0,0 +1,50 @@
+import { AxiosGQL } from "../../GraphQL/AxiosGQL";
+import { Hex, IAttestationResponse, ICommunityResponse, ICommunityAdminsResponse, IGrantResponse, IProjectResponse, ISearchResponse, IProjectMilestoneResponse } from "./types";
+export declare class GapIndexerApi extends AxiosGQL {
+    constructor(url: string);
+    attestation(uid: Hex): Promise<import("axios").AxiosResponse<IAttestationResponse, any>>;
+    attestations(schemaUID: string, search?: string): Promise<import("axios").AxiosResponse<IAttestationResponse[], any>>;
+    attestationsOf(schemaUID: string, attester: Hex): Promise<import("axios").AxiosResponse<IAttestationResponse[], any>>;
+    /**
+     * Community
+     */
+    communities(search?: string): Promise<import("axios").AxiosResponse<ICommunityResponse[], any>>;
+    communitiesOf(address: Hex, withGrants: boolean): Promise<import("axios").AxiosResponse<ICommunityResponse[], any>>;
+    adminOf(address: Hex): Promise<import("axios").AxiosResponse<ICommunityResponse[], any>>;
+    communityBySlug(slug: string): Promise<import("axios").AxiosResponse<ICommunityResponse, any>>;
+    communityAdmins(uid: Hex): Promise<import("axios").AxiosResponse<ICommunityAdminsResponse, any>>;
+    /**
+     * Project
+     */
+    projectBySlug(slug: string): Promise<import("axios").AxiosResponse<IProjectResponse, any>>;
+    search(query: string): Promise<import("axios").AxiosResponse<ISearchResponse, any>>;
+    searchProjects(query: string): Promise<import("axios").AxiosResponse<IProjectResponse[], any>>;
+    projects(name?: string): Promise<import("axios").AxiosResponse<IProjectResponse[], any>>;
+    projectsOf(grantee: Hex): Promise<import("axios").AxiosResponse<IProjectResponse[], any>>;
+    projectMilestones(uidOrSlug: string): Promise<import("axios").AxiosResponse<IProjectMilestoneResponse[], any>>;
+    /**
+     * Grantee
+     */
+    grantee(address: Hex): Promise<import("axios").AxiosResponse<any, any>>;
+    grantees(): Promise<import("axios").AxiosResponse<{
+        [key: `0x${string}`]: {
+            grants: number;
+            projects: number;
+        };
+    }, any>>;
+    /**
+     * Grant
+     */
+    grantsOf(grantee: Hex, withCommunity?: boolean): Promise<import("axios").AxiosResponse<IGrantResponse[], any>>;
+    grantsFor(uid: string, withCommunity?: boolean): Promise<import("axios").AxiosResponse<IGrantResponse[], any>>;
+    grantsForExtProject(projectExtId: string): Promise<import("axios").AxiosResponse<IGrantResponse[], any>>;
+    grantBySlug(slug: Hex): Promise<import("axios").AxiosResponse<IGrantResponse, any>>;
+    grantsByCommunity(uid: Hex, page?: number, pageLimit?: number): Promise<import("axios").AxiosResponse<{
+        data: IGrantResponse[];
+    }, any>>;
+    /**
+     * Milestone
+     */
+    milestonesOf(uid: Hex): Promise<import("axios").AxiosResponse<any, any>>;
+    slugExists(slug: string): Promise<boolean>;
+}
---
+++ core/class/karma-indexer/api/types.d.ts
@@ -0,0 +1,266 @@
+export type Hex = `0x${string}`;
+export type JSONStr = string;
+export type ExternalLink = {
+    type: string;
+    url: string;
+};
+export interface ITag {
+    name: string;
+}
+export interface IAttestationResponse {
+    id: string;
+    uid: Hex;
+    schemaUID: Hex;
+    refUID: Hex;
+    attester: Hex;
+    recipient: Hex;
+    revoked: boolean;
+    revocationTime?: number;
+    createdAt: any;
+    updatedAt: any;
+    chainID: number;
+    type: string;
+    data: any;
+    decodedDataJson: JSONStr;
+    isOffchain: any;
+    revocable: any;
+    schemaId: Hex;
+}
+export interface ISummaryProject {
+    title: string;
+    slug?: string;
+    uid: Hex;
+}
+export interface IMilestoneCompleted extends IAttestationResponse {
+    type: "MilestoneStatus";
+    data: {
+        type: "approved" | "rejected" | "completed";
+        reason?: string;
+        proofOfWork?: string;
+    };
+}
+export interface IMilestoneResponse extends IAttestationResponse {
+    type: "Milestone";
+    completed?: IMilestoneCompleted;
+    approved?: IMilestoneCompleted;
+    rejected?: IMilestoneCompleted;
+    verified?: IMilestoneCompleted[];
+    data: {
+        title: string;
+        description: string;
+        endsAt: number;
+        startsAt?: number;
+        type: "milestone";
+        priority?: number;
+    };
+}
+export interface IGrantUpdateStatus extends IAttestationResponse {
+    type: `grant-update-${IStatus}`;
+    reason?: string;
+    data: {
+        type: "approved" | "rejected" | "completed";
+        reason?: string;
+    };
+}
+export interface IGrantUpdate extends IAttestationResponse {
+    data: {
+        text: string;
+        title: string;
+        type: "grant-update";
+        proofOfWork?: string;
+    };
+    verified?: IGrantUpdateStatus[];
+}
+export interface IProjectUpdateStatus extends IAttestationResponse {
+    type: `project-update-${IStatus}`;
+    reason?: string;
+    data: {
+        type: "approved" | "rejected" | "completed";
+        reason?: string;
+    };
+}
+export interface IProjectUpdate extends IAttestationResponse {
+    data: {
+        title: string;
+        text: string;
+        startDate?: Date;
+        endDate?: Date;
+        grants?: string[];
+        indicators?: {
+            name: string;
+            indicatorId: string;
+        }[];
+        deliverables?: {
+            name: string;
+            proof: string;
+            description: string;
+        }[];
+        type: "project-update";
+    };
+    verified?: IProjectUpdateStatus[];
+}
+export interface IProjectMilestoneStatus extends IAttestationResponse {
+    type: `project-milestone-verified`;
+    reason?: string;
+    data: {
+        type: "approved" | "rejected" | "completed";
+        reason?: string;
+    };
+}
+export interface IProjectMilestoneResponse extends IAttestationResponse {
+    data: {
+        text: string;
+        title: string;
+        type: "project-milestone";
+    };
+    verified?: IProjectMilestoneStatus[];
+    completed?: IMilestoneCompleted;
+}
+export interface IProjectPointer extends IAttestationResponse {
+    data: {
+        ogProjectUID: string;
+        type: "project-pointer";
+    };
+}
+export interface IGrantDetails extends IAttestationResponse {
+    type: "GrantDetails";
+    data: {
+        title: string;
+        amount: string;
+        description: string;
+        proposalURL: string;
+        assetAndChainId?: [Hex, number];
+        payoutAddress: Hex;
+        questions: {
+            type: string;
+            query: string;
+            explanation: string;
+        }[];
+        startDate: number;
+        programId?: string;
+        type: "grant-details";
+        fundUsage?: string;
+    };
+}
+export interface IGrantResponse extends IAttestationResponse {
+    type: "Grant";
+    data: {
+        communityUID: Hex;
+    };
+    details?: IGrantDetails;
+    milestones: IMilestoneResponse[];
+    completed?: IGrantUpdate;
+    project: ISummaryProject;
+    updates: IGrantUpdate[];
+    community: ICommunityResponse;
+    members: Hex[];
+    categories?: string[];
+    externalAddresses?: {
+        [key: string]: string;
+    };
+    external?: {
+        [key: string]: string[];
+    };
+}
+export interface IMemberDetails extends IAttestationResponse {
+    name: string;
+    profilePictureURL: string;
+}
+export interface IMemberOf extends IAttestationResponse {
+    type: "MemberOf";
+    data: {
+        memberOf: true;
+    };
+    details?: IMemberDetails;
+}
+export interface IProjectDetails extends IAttestationResponse {
+    type: "ProjectDetails";
+    data: {
+        title: string;
+        description: string;
+        problem?: string;
+        solution?: string;
+        missionSummary?: string;
+        locationOfImpact?: string;
+        imageURL: string;
+        links?: ExternalLink[];
+        tags?: ITag[];
+        slug?: string;
+        type: "project-details";
+        businessModel?: string;
+        stageIn?: string;
+        raisedMoney?: string;
+        pathToTake?: string;
+    };
+}
+type IStatus = "verified";
+export interface IProjectImpactStatus extends IAttestationResponse {
+    type: `project-impact-${IStatus}`;
+    reason?: string;
+}
+export interface IProjectImpact extends IAttestationResponse {
+    type: "ProjectImpact";
+    data: {
+        work: string;
+        impact: string;
+        proof: string;
+        startedAt?: number;
+        completedAt: number;
+        type: "project-impact";
+    };
+    verified: IProjectImpactStatus[];
+}
+export interface IProjectEndorsement extends IAttestationResponse {
+    type: "ProjectEndorsement";
+    data: {
+        comment?: string;
+        type?: "project-endorsement";
+    };
+}
+export interface IProjectResponse extends IAttestationResponse {
+    type: "Project";
+    data: {
+        project: true;
+    };
+    details?: IProjectDetails;
+    members: IMemberOf[];
+    grants: IGrantResponse[];
+    grantee: any;
+    impacts: IProjectImpact[];
+    updates: IProjectUpdate[];
+    pointers: IProjectPointer[];
+    symlinks: Hex[];
+    endorsements: IProjectEndorsement[];
+    milestones: IProjectMilestoneResponse[];
+}
+export interface ICommunityDetails extends IAttestationResponse {
+    type: "CommunityDetails";
+    data: {
+        name: string;
+        description: string;
+        imageURL: string;
+        links: ExternalLink[];
+        slug?: string;
+    };
+}
+export interface ICommunityResponse extends IAttestationResponse {
+    type: "Community";
+    data: {
+        community: true;
+    };
+    details?: ICommunityDetails;
+    grants: IGrantResponse[];
+}
+export interface ICommunityAdminsResponse {
+    id: string;
+    admins: {
+        user: {
+            id: string;
+        };
+    }[];
+}
+export interface ISearchResponse {
+    projects: IProjectResponse[];
+    communities: ICommunityResponse[];
+}
+export {};
