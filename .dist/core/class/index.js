de6f0489459393ddb3b56d436df339729d0a4db7
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2026-02-11 12:40:41 -0300

    chore: add sdk dist artifacts for local app testing

---
+++ .dist/core/class/index.js
@@ -0,0 +1,26 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+__exportStar(require("./Attestation"), exports);
+__exportStar(require("./GAP"), exports);
+__exportStar(require("./GapSchema"), exports);
+__exportStar(require("./Schema"), exports);
+__exportStar(require("./SchemaError"), exports);
+__exportStar(require("./entities"), exports);
+__exportStar(require("./Fetcher"), exports);
+__exportStar(require("./karma-indexer/GapIndexerClient"), exports);
+__exportStar(require("./remote-storage/IpfsStorage"), exports);
+__exportStar(require("./remote-storage/RemoteStorage"), exports);
